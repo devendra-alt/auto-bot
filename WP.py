d2652293b69c160c5c106480aef1429e2b618796
Devendra <devendramulewa171@gmail.com>
2021-05-25 13:11:13 +0530

    bot 0.2

--- WP.py
+++ WP.py
@@ -1,50 +1,77 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-added_options = webdriver.ChromeOptions()
-added_options.add_argument(
-    '--user-data-dir= /home/devendra/.config/google-chrome/Default')
-added_options.add_argument('--profile-directory=Default')
-driver = webdriver.Chrome(
-    '/home/devendra/SDP/whatsapp_fry/chromedriver', options=added_options)
-driver.get('https://web.whatsapp.com')
-print("Scan QR code and hit Enter")
-input()
-print("Logged in")
-time.sleep(1)
-contact = input('enter name of contact : ')
-msg_type = input(
-    'enter 1 for text message \nenter 2 for image \nenter your choice : ')
+driver = ""
 
-search_box = driver.find_element_by_xpath(
-    "//*[@id='side']/div[1]/div/label/div/div[2]")
-search_box.send_keys(contact)
-search_box.send_keys(Keys.ENTER)
 
-if(msg_type == 1):
+def main():
+    added_options = webdriver.ChromeOptions()
+    added_options.add_argument(
+        '--user-data-dir= /home/devendra/.config/google-chrome/Default')
+    added_options.add_argument('--profile-directory=Default')
+    driver = webdriver.Chrome(
+        '/home/devendra/SDP/whatsapp_fry/chromedriver', options=added_options)
+    driver.get('https://web.whatsapp.com')
+    print("Scan QR code and hit Enter")
+    input()
+    print("Logged in success")
+    time.sleep(1)
+    send_message()
+
+
+def send_message():
+    contact = input('enter name of contact : ')
+    find_contact(contact)
+
+    print("enter 1 for text message")
+    print("enter 2 for image")
+
+    msg_type = int(input("enter your choice : "))
+
+    if(msg_type == 1):
+        text_message()
+    elif(msg_type == 2):
+        image_message()
+
+
+def find_contact(contact):
+    search_box = driver.find_element_by_xpath(
+        "//*[@id='side']/div[1]/div/label/div/div[2]")
+    search_box.send_keys(contact)
+    search_box.send_keys(Keys.ENTER)
+
+
+def text_message():
     msg = input('enter the message : ')
-    no_of_msg = input('enter number of message : ')
+    no_of_msg = int(input('enter number of message : '))
     msg_box = driver.find_element_by_xpath(
         "//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
     time.sleep(1)
     for i in range(no_of_msg):
         msg_box.send_keys(msg)
-        msg_box.send_keys(Keys.ENTER)
-        time.sleep(0.4)
-else:
+        msg_box.send_keys
+
+
+def image_message():
     file_path = (input("enter file path : "))
     caption = (input("enter caption : "))
     attachment_section = driver.find_element_by_xpath(
         '//*[@id="main"]/footer/div[1]/div[1]/div[2]/div/div')
+    attachment_section.click()
     image_box = driver.find_element_by_xpath(
-        '//*[@id="main"]/footer/div[1]/div[1]/div[2]/div/span/div[1]/div/ul/li[1]/button/span')
-    image_box.send_Keys(file_path)
+        '//input[@accept="image/*,video/mp4,video/3gpp,video/quicktime"]')
+    image_box.send_keys(file_path)
     time.sleep(3)
     caption_box = driver.find_element_by_xpath(
         '//*[@id="app"]/div[1]/div[1]/div[2]/div[2]/span/div[1]/span/div[1]/div/div[2]/div[1]/span/div/div[2]/div/div[3]/div[1]/div[2]')
-    caption_box.send_keys(msg)
+    caption_box.send_keys(caption)
     send_button = driver.find_element_by_xpath(
         '//*[@id="app"]/div[1]/div[1]/div[2]/div[2]/span/div[1]/span/div[1]/div/div[2]/span/div/div/span')
     send_button.click()
-time.sleep(15)
+    print("successful")
+
+
+time.sleep(120)
 driver.quit()
+
+main()
